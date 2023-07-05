db649b0c453c00ff5c258a29176fe0a31a58eb54
betuldagli <125824370+betuldagli@users.noreply.github.com>
2023-07-05 20:56:38 +0300

    Add files via upload

---
+++ SayiTahmin.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Random;
+/**Java dilinde programın 0-100 arasında rastgele seçtiği bir sayıyı kullanıcının tahmin etmesini istediğimiz bir "Sayı Tahmin Oyunu" yapıyoruz.*/
+public class SayiTahmin {
+    public static void main(String[] args) {
+        Scanner girdi = new Scanner(System.in);
+        Random rand = new Random();
+        int number = rand.nextInt(100);
+        int right=5;
+        int tahmin;
+
+        System.out.println(number);
+
+        while(right>0){
+            System.out.println("Sayı giriniz:");
+            tahmin=girdi.nextInt();
+
+            if(tahmin==number){
+                System.out.println("Tahmininiz doğru.");
+                break;
+            }
+            else{
+                if(right!=1){
+                System.out.println("Yanlış tahmin tekrar deneyiniz.");
+                }
+                else{
+                    System.out.println("Tahmininiz yanlış,kaybettiniz.");
+                }
+                right--;
+        }
+    }
+  }
+}
